dedf8ab3fd10defb5aa54939a4b402d666270120
Ali Lee <fcqz121314@gmail.com>
2026-08-29 07:51:19 -0400

    feat: render Facebook default target setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
+import FacebookTargetPanel from './FacebookTargetPanel'
 import './styles.css'
 import './publish.css'
+import './facebook-target.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <FacebookTargetPanel />
     <App />
   </React.StrictMode>,
 )
