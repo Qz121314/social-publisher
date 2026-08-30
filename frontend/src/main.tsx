56c7c56f76afcc05556aeec292ac2244562a9b13
Ali Lee <fcqz121314@gmail.com>
2026-08-29 22:36:38 -0400

    phase3: load domain workspace styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import './facebook-target.css'
 import './admin-shell.css'
 import './facebook-flow-config.css'
 import './app/v1-shell.css'
+import './app/phase3.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
